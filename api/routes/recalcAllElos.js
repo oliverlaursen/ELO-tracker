1316cc68b22870593f751b326b6ced65b1c54130
au689257 <ral8310@gmail.com>
2023-03-20 11:45:05 +0100

    Fixed elochange when recalculating

--- api/routes/recalcAllElos.js
+++ api/routes/recalcAllElos.js
@@ -20,6 +20,8 @@ router.get('/', async function (req, res, next) {
                 const [p1elochange, p2elochange] = elo_calc.calcEloChange(p1elo, p2elo, parseInt(match.player1Goals), parseInt(match.player2Goals));
                 match.player1Elo = p1elo+p1elochange;
                 match.player2Elo = p2elo+p2elochange;
+                match.player1EloChange = p1elochange;
+                match.player2EloChange = p2elochange;
                 elo_calc.updatePlayersElo(match.player1,match.player2,p1elochange,p2elochange,users);
             });
         await fs.promises.writeFile('data/matches.json', JSON.stringify(matches));
