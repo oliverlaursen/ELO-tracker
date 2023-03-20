fcbb6a11388d97916f46ebb6555ed8edbf6ed09e
au689257 <ral8310@gmail.com>
2023-03-20 14:48:18 +0100

    Added data files for vercel

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ async function getIPAddress() {
 	return data.ip;
 }
 
-//const PUBLIC_IP = await getIPAddress()
-const PUBLIC_IP = "localhost"
+const PUBLIC_IP = await getIPAddress()
+//const PUBLIC_IP = "localhost"
 
 function GoalSlider(props) {
 	const handleChange = (event) => {
